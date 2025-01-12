c9c4a2a15d1cc90e548556aa9162ce6b60f9235a
BugHunter-X <askzenai@gmail.com>
2025-01-12 01:54:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// Some Tailwind CSS classes are not applied in a specific component.
+
+<div className="bg-red-500 p-4">
+  <p>This text should be red and have padding.</p>
+</div>
+```
